7ccbdf73b4999b40c23eff638551252530e03f6c
yeon-jae <lisanamyj99@gmail.com>
2025-01-19 01:27:21 +0900

    [YJ] 0119 문제 풀이

---
+++ YeonJae/BOJ/BFS&DFS/DFS와 BFS.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def dfs(n):
+    print(n, end=' ')
+    visited[n] = True
+
+    for i in graph[n]:
+        if not visited[i]:
+            dfs(i)
+
+
+def bfs(n):
+    visited[n] = True
+    queue = deque([n])
+
+    while queue:
+        q = queue.popleft()
+        print(q, end=' ')
+
+        for i in graph[q]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+
+# main
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+
+for i in range(n + 1):
+    graph[i].sort()
+
+
+visited = [False] * (n + 1)
+dfs(v)
+print()
+visited = [False] * (n + 1)
+bfs(v)
