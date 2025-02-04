9c3b21b86341b966e9f6fdba61c041ed5109b1d2
yeon-jae <lisanamyj99@gmail.com>
2025-02-04 15:33:01 +0900

    Yeonjae/Programmers/LV1

---
+++ YeonJae/Programmers/LV1/두개뽑아서더하기.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    answer=set()
+    numbers.sort()
+    for i in range(len(numbers)):
+        for j in range(i+1,len(numbers)):
+            answer.add(numbers[i]+numbers[j])
+    return sorted(answer) 
