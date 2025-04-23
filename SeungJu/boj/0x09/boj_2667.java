cc851bbee6aafca24ec1ee8365351b0a2181063c
seunzu <0123suh@gmail.com>
2025-04-23 23:57:26 +0900

    SeungJu/boj/bfs

---
+++ SeungJu/boj/0x09/boj_2667.java
@@ -0,0 +1,67 @@
+package bfs;
+
+import java.io.*;
+import java.util.*;
+
+// 단지 번호 붙이기
+public class boj_2667 {
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static int bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        int size = 1;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cur[0] + dx[dir];
+                int ny = cur[1] + dy[dir];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    q.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    size++;
+                }
+            }
+        }
+
+        return size;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    arr.add(bfs(i, j));
+                }
+            }
+        }
+
+        Collections.sort(arr);
+        System.out.println(arr.size());
+        for (int i : arr) System.out.println(i);
+    }
+}
