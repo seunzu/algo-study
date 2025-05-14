2161f7d6ae1aaca22ac22bcee3d631e40abc2022
yeon-jae <lisanamyj99@gmail.com>
2025-05-14 14:45:13 +0900

    Yeonjae/Programmers/기초문제

--- YeonJae/Programmers/LV0/순서바꾸기.py
+++ YeonJae/Programmers/LV0/순서바꾸기.py
@@ -5,3 +5,15 @@ def solution(num_list, n):
     for i in range(0,n):
         answer.append(num_list[i])
     return answer
+
+
+#다른 풀이 
+def solution(num_list, n):
+    answer = []
+    f_ans=[]
+    ans=[]
+    for i in range(len(num_list)):
+        if i<n:
+            ans.append(num_list[i]);
+        else: f_ans.append(num_list[i])
+    return f_ans+ans
--- YeonJae/Programmers/LV0/중복된문자제거.py
+++ YeonJae/Programmers/LV0/중복된문자제거.py
@@ -1,2 +1,13 @@
 def solution(my_string):
     return ''.join(dict.fromkeys(my_string))
+
+
+#중복문자 제거 
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i in answer:
+            continue
+        else:
+            answer+=i
+    return answer
