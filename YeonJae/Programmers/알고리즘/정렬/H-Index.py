22f062838cd4c4fd1b398f1f1d1e4b947ea69286
yeon-jae <lisanamyj99@gmail.com>
2025-05-17 16:12:17 +0900

    Yeonjae/change folder name

--- YeonJae/Programmers/알고리즘/정렬/H-Index.py
+++
@@ -1,8 +0,0 @@
-def solution(citations):
-    cnt=0
-    citation=sorted(citations,reverse=True)
-    for i,c in enumerate(citation):
-        if c>=i+1: #인용횟수가 논문개수 이상일때
-            cnt=i+1
-        else: break
-    return cnt
--- YeonJae/Programmers/알고리즘/정렬/K번째수.py
+++
@@ -1,8 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for command in commands:
-        i,j,k=command
-        sliced=sorted(array[i-1:j])
-        answer.append(sliced[k-1])
-    return answer
-    
