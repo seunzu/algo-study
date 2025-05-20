b9f6911240669aeed5a1c71c5f5818c7a090d90f
seunzu <0123suh@gmail.com>
2025-05-20 23:46:02 +0900

    SeungJu/BOJ

---
+++ SeungJu/boj/beginner/boj_1181.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+// 단어 정렬
+public class boj_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        String[] arr = set.toArray(new String[0]);
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) return o1.compareTo(o2);
+                else return o1.length() - o2.length();
+            }
+        });
+
+        for (String s : arr) sb.append(s).append("\n");
+        System.out.println(sb);
+    }
+}
---
+++ SeungJu/boj/beginner/boj_5347.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+// LCM
+public class boj_5347 {
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        else return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(lcm(a, b)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
