0c620ae9007ab853b2d3923fb5ee715b892228ff
seunzu <0123suh@gmail.com>
2025-05-28 21:52:01 +0900

    SeungJu/BOJ

---
+++ SeungJu/boj/beginner/boj_16194.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+// 카드 구매하기 2
+public class boj_16194 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        int[] cards = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {a
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + cards[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
