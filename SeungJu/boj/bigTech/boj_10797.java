9eb12c66cf8feeb17022f5158bf9115f47ba931a
seunzu <0123suh@gmail.com>
2025-06-06 14:18:04 +0900

    SeungJu/BOJ

---
+++ SeungJu/boj/bigTech/boj_10797.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+// 10부제
+public class boj_10797 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 5; i++) {
+            if (Integer.parseInt(st.nextToken()) == n) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
---
+++ SeungJu/boj/bigTech/boj_2506.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+// 점수 계산
+public class boj_2506 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int score = 0;
+        int cur = 0;
+
+        for (int i = 0; i < N; i++) {
+            int M = Integer.parseInt(st.nextToken());
+            if (M == 1) {
+                cur++;
+                score += cur;
+            } else cur = 0;
+        }
+
+        System.out.println(score);
+    }
+}
