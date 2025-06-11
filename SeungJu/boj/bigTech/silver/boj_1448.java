c32bff140e05dd3b0fabb1e87d9e79e94a5ba8d5
seunzu <0123suh@gmail.com>
2025-06-11 16:13:47 +0900

    SeungJu/BOJ

---
+++ SeungJu/boj/bigTech/silver/boj_1448.java
@@ -0,0 +1,33 @@
+package silver;
+
+import java.io.*;
+import java.util.*;
+
+// 삼각형 만들기
+public class boj_1448 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int len = -1;
+        for (int i = N - 1; i >= 2; i--) {
+            int a = arr[i];
+            int b = arr[i - 1];
+            int c = arr[i - 2];
+
+            if (b + c > a) {
+                len = a + b + c;
+                break;
+            }
+        }
+
+        System.out.println(len);
+    }
+}
