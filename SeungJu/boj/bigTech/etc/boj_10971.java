3f2521922649c19f804430acf7b523af764c0cdb
seunzu <0123suh@gmail.com>
2025-06-14 23:50:43 +0900

    SeungJu/BOJ

---
+++ SeungJu/boj/bigTech/etc/boj_10971.java
@@ -0,0 +1,50 @@
+package etc;
+
+import java.io.*;
+import java.util.*;
+
+// 외판원 순회 2
+public class boj_10971 {
+    static int N;
+    static int[][] W;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
+
+    static void dfs(int start, int now, int depth, int cost) {
+        if (depth == N && W[now][start] != 0) {
+            min = Math.min(min, cost + W[now][start]);
+            return;
+        }
+
+        for (int next = 0; next < N; next++) {
+            if (!visited[next] && W[now][next] != 0) {
+                visited[next] = true;
+                dfs(start, next, depth + 1, cost + W[now][next]);
+                visited[next] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        W = new int[N][N];
+        visited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                W[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            visited[i] = true;
+            dfs(i, i, 1, 0);
+            visited[i] = false;
+        }
+
+        System.out.println(min);
+    }
+}
