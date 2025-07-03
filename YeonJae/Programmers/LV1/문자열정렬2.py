07611773cf2dab0af1c741f5b397d4f0341904df
yeon-jae <lisanamyj99@gmail.com>
2025-07-03 15:01:21 +0900

    Yeonjae/Programmers/문제풀이

---
+++ YeonJae/Programmers/LV1/문자열정렬2.py
@@ -0,0 +1,6 @@
+def solution(my_string):
+    answer = ''
+    answer=my_string.lower()
+    answer=sorted(answer)
+    return ''.join(answer)
+   
--- YeonJae/Programmers/LV1/크기가작은부분문자열.py
+++ YeonJae/Programmers/LV1/크기가작은부분문자열.py
@@ -1,14 +1,24 @@
 def solution(t, p):
-    answer = 0
-    idx=len(p)
-    arr=[]
-    cnt=0
-    for i in range(len(t)-idx+1):
-        arr.append(t[i:i+idx])
-    for i in range(len(arr)):
-        if int(arr[i])<=int(p):
-            cnt+=1
-    return cnt
+  answer = 0
+  p_len = len(p)
+  t_len = len(t)
+  p = int(p)
+  for i in range(0,t_len+1-p_len):
+      if int(t[i:i+p_len]) <= p:
+          answer+=1
+  return answer
+
+# def solution(t, p):
+#     answer = 0
+#     idx=len(p)
+#     arr=[]
+#     cnt=0
+#     for i in range(len(t)-idx+1):
+#         arr.append(t[i:i+idx])
+#     for i in range(len(arr)):
+#         if int(arr[i])<=int(p):
+#             cnt+=1
+#     return cnt
 
 
 
