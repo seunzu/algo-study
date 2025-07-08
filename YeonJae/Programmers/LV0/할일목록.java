f0873d467ee0b58398fe263c83aac278e46291f8
yeon-jae <lisanamyj99@gmail.com>
2025-07-08 15:53:04 +0900

    Yeonjae/Programmers/java 문제풀이

---
+++ YeonJae/Programmers/LV0/할일목록.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> result= new ArrayList<>();
+        for(int i=0; i<todo_list.length; i++){
+            if (!finished[i]){
+                result.add(todo_list[i]);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
