da21ce53de9aad6502055ae13e1902b50c33caa4
seunzu <0123suh@gmail.com>
2025-08-20 11:53:30 +0900

    SeungJu/BOJ/실버

---
+++ SeungJu/boj/bigTech/silver/boj_10867.java
@@ -0,0 +1,26 @@
+package silver;
+
+import java.io.*;
+import java.util.*;
+
+// 중복 빼고 정렬하기
+public class boj_10867 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        Set<Integer> set = new TreeSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i : set) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
