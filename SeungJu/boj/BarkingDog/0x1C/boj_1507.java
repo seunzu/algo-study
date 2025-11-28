56053e69900f079255018fa4ba27e8dfa5bfe290
seunzu <0123suh@gmail.com>
2025-11-28 10:21:03 +0900

    SeungJu/BOJ/floydWarshall

---
+++ SeungJu/boj/BarkingDog/0x1C/boj_1507.java
@@ -0,0 +1,21 @@
+package floydWarshall;
+
+import java.io.*;
+import java.util.*;
+
+// 궁금한 민호
+public class boj_1507 {
+    static int N;
+    static int[][] time;
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+        }
+    }
+}
---
+++ SeungJu/boj/BarkingDog/0x1C/boj_1956.java
@@ -0,0 +1,53 @@
+package floydWarshall;
+
+import java.io.*;
+import java.util.*;
+
+// 운동
+public class boj_1956 {
+    static int V, E;
+    static int[][] dist;
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        dist = new int[V + 1][V + 1];
+        for (int i = 1; i <= V; i++) {
+            Arrays.fill(dist[i], INF);
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = c;
+        }
+
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        int ans = INF;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i != j) {
+                    ans = Math.min(ans, dist[i][j] + dist[j][i]);
+                }
+            }
+        }
+
+        System.out.println(ans == INF ? -1 : ans);
+    }
+}
