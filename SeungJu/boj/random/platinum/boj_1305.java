c4eb6f99f061e446cad572e75079c5124897381c
seunzu <0123suh@gmail.com>
2025-12-26 23:13:08 +0900

    SeungJu/BOJ/플래티넘

---
+++ SeungJu/boj/random/platinum/boj_1305.java
@@ -0,0 +1,35 @@
+package platinum;
+
+import java.io.*;
+import java.util.*;
+
+// 광고
+public class boj_1305 {
+    static int L;
+    static String text;
+
+    static int[] getPi() {
+        int[] pi = new int[text.length()];
+        int j = 0;
+
+        for (int i = 1; i < text.length(); i++) {
+            while (j > 0 && text.charAt(i) != text.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (text.charAt(i) == text.charAt(j)) {
+                pi[i] = ++j;
+            }
+        }
+        return pi;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        L = Integer.parseInt(br.readLine());
+        text = br.readLine();
+
+        int[] pi = getPi();
+        System.out.println(L - pi[L - 1]);
+    }
+}
---
+++ SeungJu/boj/random/platinum/boj_1786.java
@@ -0,0 +1,61 @@
+package platinum;
+
+import java.io.*;
+import java.util.*;
+
+// 찾기
+public class boj_1786 {
+    static String text, pattern;
+
+    static int[] getPi() {
+        int[] pi = new int[pattern.length()];
+        int j = 0;
+
+        for (int i = 1; i < pattern.length(); i++) {
+            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                pi[i] = ++j;
+            }
+        }
+        return pi;
+    }
+
+    static List<Integer> kmp() {
+        int[] pi = getPi();
+        List<Integer> result = new ArrayList<>();
+
+        int j = 0;
+        for (int i = 0; i < text.length(); i++) {
+            while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
+                j = pi[j - 1];
+            }
+
+            if (text.charAt(i) == pattern.charAt(j)) {
+                if (j == pattern.length() - 1) {
+
+                    result.add(i - pattern.length() + 2);
+                    j = pi[j];
+                } else {
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        text = br.readLine();
+        pattern = br.readLine();
+
+        List<Integer> result = kmp();
+        sb.append(result.size()).append('\n');
+        for (int r : result) {
+            sb.append(r).append(' ');
+        }
+        System.out.println(sb);
+    }
+}
