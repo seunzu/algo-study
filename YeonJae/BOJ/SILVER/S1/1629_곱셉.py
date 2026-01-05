a135fe9b95321f1c0b4b0456ed76ce1835d25b8c
yeon-jae <lisanamyj99@gmail.com>
2026-01-05 16:58:41 +0900

    Yeonjae/BOJ/SILVER/S1/1629_곱셉.py

---
+++ YeonJae/BOJ/SILVER/S1/1629_곱셉.py
@@ -0,0 +1,14 @@
+def power(a, b, c):
+    if b == 1:
+        return a % c
+    
+    temp = power(a, b // 2, c)
+    
+    if b % 2 == 0:
+        return (temp * temp) % c
+    else:
+        return (temp * temp * a) % c
+
+
+A, B, C = map(int, input().split())
+print(power(A, B, C))
