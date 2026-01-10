9a9eb62033a71bc3fc74886105ee50dd900c43bd
seunzu <0123suh@gmail.com>
2026-01-11 00:07:44 +0900

    SeungJu/programmers/lv2

--- SeungJu/programmers/Lv2/n진수게임/Solution.java
+++ SeungJu/programmers/Lv2/n진수게임/Solution.java
@@ -1,4 +1,19 @@
 package n진수게임;
 
-public class Solution {
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+
+        while (sb.length() < t * m) {
+            sb.append(Integer.toString(num++, n).toUpperCase());
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            answer.append(sb.charAt(i * m + (p - 1)));
+        }
+
+        return answer.toString();
+    }
 }
