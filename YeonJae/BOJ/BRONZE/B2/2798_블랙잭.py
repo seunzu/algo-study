d83e2b9d1e2b872bd4dcf120ace2e0d115e097d4
yeon-jae <lisanamyj99@gmail.com>
2026-01-14 11:13:01 +0900

    Yeonjae/BOJ/BRONZE/B2/2798_블랙잭.py

--- YeonJae/BOJ/BRONZE/B2/2798_블랙잭.py
+++ YeonJae/BOJ/BRONZE/B2/2798_블랙잭.py
@@ -2,6 +2,7 @@ n,m=list(map(int,input().split()))
 card=list(map(int,input().split()))
 anw=0 
 
+card_list=[]
 total_sum=0
 length=len(card)
 
@@ -11,7 +12,7 @@ for i in range(0, length):
     for j in range(i + 1, length):
         for k in range(j + 1, length):
             sum_card = card_list[i] + card_list[j] + card_list[k]
-            if sum_card <= M:
+            if sum_card <= m:
                 result = max(anw, sum_card)
 
 print(anw)
