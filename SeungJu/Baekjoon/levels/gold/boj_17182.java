a9342b42f9c1097f378923d88ea563154baf3e6a
seunzu <0123suh@gmail.com>
2026-04-06 23:46:30 +0900

    SeungJu/BOJ/골드

--- SeungJu/Baekjoon/levels/gold/boj_17182.java
+++ SeungJu/Baekjoon/levels/gold/boj_17182.java
@@ -10,6 +10,21 @@ public class boj_17182 {
     static boolean[] visited;
     static int ans = Integer.MAX_VALUE;
 
+    static void dfs(int start, int cost, int cnt) {
+        if (cnt == N) {
+            ans = Math.min(ans, cost);
+            return;
+        }
+
+        for (int next = 0; next < N ; next++) {
+            if (!visited[next]) {
+                visited[next] = true;
+                dfs(next, cost + dist[start][next], cnt + 1);
+                visited[next] = false;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,6 +33,8 @@ public class boj_17182 {
         K = Integer.parseInt(st.nextToken());
 
         dist = new int[N][N];
+        visited = new boolean[N];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -25,6 +42,17 @@ public class boj_17182 {
             }
         }
 
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
 
+        visited[K] = true;
+        dfs(K, 0, 1);
+
+        System.out.println(ans);
     }
 }
