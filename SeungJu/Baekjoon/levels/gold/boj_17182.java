204030b6b33c5b4e69b3334af2a7a480c1b14ed7
seunzu <0123suh@gmail.com>
2026-04-06 23:43:36 +0900

    SeungJu/BOJ/골드

---
+++ SeungJu/Baekjoon/levels/gold/boj_17182.java
@@ -0,0 +1,30 @@
+package gold;
+
+import java.io.*;
+import java.util.*;
+
+// 우주 탐사선
+public class boj_17182 {
+    static int N, K;
+    static int[][] dist;
+    static boolean[] visited;
+    static int ans = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        dist = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                dist[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+    }
+}
