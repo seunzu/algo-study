eeb3dcdf961e58e27a4aca21fa84e4ab0bcb5283
seunzu <0123suh@gmail.com>
2026-04-14 00:22:03 +0900

    SeungJu/BOJ

--- SeungJu/Baekjoon/BarkingDog/0x09/boj_10026.java
+++ SeungJu/Baekjoon/BarkingDog/0x09/boj_10026.java
@@ -6,43 +6,29 @@ import java.util.*;
 // 적록색약
 public class boj_10026 {
     static int N;
-    static char[][] map;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
-    public static void bfs(int x, int y, boolean isBlind) {
-        Queue<int[]> q = new LinkedList<>();
+    static void bfs(int x, int y, char[][] board) {
+        Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{x, y});
         visited[x][y] = true;
 
-        char color = map[x][y];
+        char color = board[x][y];
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
 
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur[0] + dx[dir];
-                int ny = cur[1] + dy[dir];
+            for (int d = 0; d < 4; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
 
                 if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if (visited[nx][ny]) continue;
 
-                char nextColor = map[nx][ny];
-
-                if (isBlind) {
-                    if ((color == 'R' || color == 'G') && (nextColor == 'R' || nextColor == 'G')) {
-                        visited[nx][ny] = true;
-                        q.offer(new int[]{nx, ny});
-                    } else if (color == 'B' && nextColor == 'B') {
-                        visited[nx][ny] = true;
-                        q.offer(new int[]{nx, ny});
-                    }
-                } else {
-                    if (color == nextColor) {
-                        visited[nx][ny] = true;
-                        q.offer(new int[]{nx, ny});
-                    }
+                if (!visited[nx][ny] && board[nx][ny] == color) {
+                    visited[nx][ny] = true;
+                    q.offer(new int[]{nx, ny});
                 }
             }
         }
@@ -52,10 +38,18 @@ public class boj_10026 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        map = new char[N][N];
 
+        char[][] board = new char[N][N];
         for (int i = 0; i < N; i++) {
-            map[i] = br.readLine().toCharArray();
+            board[i] = br.readLine().toCharArray();
+        }
+
+        char[][] blindBoard = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 'G') blindBoard[i][j] = 'R';
+                else blindBoard[i][j] = board[i][j];
+            }
         }
 
         visited = new boolean[N][N];
@@ -63,7 +57,7 @@ public class boj_10026 {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    bfs(i, j, false);
+                    bfs(i, j, board);
                     normal++;
                 }
             }
@@ -74,12 +68,13 @@ public class boj_10026 {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    bfs(i, j, true);
+                    bfs(i, j, blindBoard);
                     blind++;
                 }
             }
         }
 
         System.out.println(normal + " " + blind);
+
     }
 }
