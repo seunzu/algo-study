623a17cce298d86f81764082a7bb4334204a10df
seunzu <0123suh@gmail.com>
2026-04-13 23:19:26 +0900

    SeungJu/BOJ

--- SeungJu/Baekjoon/BarkingDog/0x09/boj_1926.java
+++ SeungJu/Baekjoon/BarkingDog/0x09/boj_1926.java
@@ -6,13 +6,13 @@ import java.util.*;
 // 그림
 public class boj_1926 {
     static int n, m;
-    static int[][] map;
+    static int[][] board;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
-    public static int bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
+    static int bfs(int x, int y) {
+        Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{x, y});
         visited[x][y] = true;
         int size = 1;
@@ -20,18 +20,19 @@ public class boj_1926 {
         while (!q.isEmpty()) {
             int[] cur = q.poll();
 
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur[0] + dx[dir];
-                int ny = cur[1] + dy[dir];
+            for (int d = 0; d < 4; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
 
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if (!visited[nx][ny] && map[nx][ny] == 1) {
-                    q.offer(new int[]{nx, ny});
+                if (!visited[nx][ny] && board[nx][ny] == 1) {
                     visited[nx][ny] = true;
+                    q.offer(new int[]{nx, ny});
                     size++;
                 }
             }
         }
+
         return size;
     }
 
@@ -42,21 +43,21 @@ public class boj_1926 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
+        board = new int[n][m];
+        visited = new boolean[n][m];
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        visited = new boolean[n][m];
         int cnt = 0;
-        int max = 0;
-
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
+                if (board[i][j] == 1 && !visited[i][j]) {
                     cnt++;
                     max = Math.max(max, bfs(i, j));
                 }
