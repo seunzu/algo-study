6c8b5e91974176c9cd8471c237ffb2db57418d41
seunzu <0123suh@gmail.com>
2026-04-14 19:20:17 +0900

    SeungJu/BOJ

--- SeungJu/Baekjoon/BarkingDog/0x09/boj_7562.java
+++ SeungJu/Baekjoon/BarkingDog/0x09/boj_7562.java
@@ -6,26 +6,26 @@ import java.util.*;
 // 나이트의 이동
 public class boj_7562 {
     static int t, l;
-    static int[][] map;
+    static int[][] board;
     static boolean[][] visited;
     static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
     static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
 
-    public static void bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
+    static void bfs(int x, int y) {
+        Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{x, y});
         visited[x][y] = true;
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
 
-            for (int dir = 0; dir < 8; dir++) {
-                int nx = cur[0] + dx[dir];
-                int ny = cur[1] + dy[dir];
+            for(int d = 0; d < 8; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
 
                 if (nx < 0 || ny < 0 || nx >= l || ny >= l) continue;
                 if (!visited[nx][ny]) {
-                    map[nx][ny] = map[cur[0]][cur[1]] + 1;
+                    board[nx][ny] = board[cur[0]][cur[1]] + 1;
                     q.offer(new int[]{nx, ny});
                     visited[nx][ny] = true;
                 }
@@ -42,7 +42,7 @@ public class boj_7562 {
         while (t-- > 0) {
             l = Integer.parseInt(br.readLine());
 
-            map = new int[l][l];
+            board = new int[l][l];
             visited = new boolean[l][l];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -54,7 +54,7 @@ public class boj_7562 {
             int y2 = Integer.parseInt(st.nextToken());
 
             bfs(x1, y1);
-            sb.append(map[x2][y2]).append("\n");
+            sb.append(board[x2][y2]).append("\n");
         }
 
         System.out.println(sb);
